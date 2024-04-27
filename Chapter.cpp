7e049adf77a95ed63f0866d1e4b7fe1f3ef321e5
“wuliangzhe” <184084180@qq.com>
2024-04-28 00:07:33 +0800

    修改项目

---
+++ Chapter.cpp
@@ -0,0 +1,20 @@
+//
+// Created by wlz20 on 2024/4/27.
+//
+
+#include "Chapter.h"
+Chapter::Chapter(int num)
+{
+    chapterNum=num;
+    ptr= nullptr;
+}
+void Chapter::SwitchChapter(int index)
+{
+    switch (index)
+    {
+        case 1:
+            break;
+        case 2:
+            break;
+    }
+}
