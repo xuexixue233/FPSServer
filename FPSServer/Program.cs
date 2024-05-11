556131b87c43a93ebb198fb2d1daf9b9068c60c0
“wuliangzhe” <184084180@qq.com>
2024-05-12 02:15:08 +0800

    添加心跳包

--- FPSServer/Program.cs
+++ FPSServer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Sockets;
+using FPSServer.db;
 using FPSServer.Net;
 
 namespace FPSServer;
@@ -7,9 +8,9 @@ class MainClass
     public static void Main (string[] args)
     {
         //数据库接口
-        // if(!DbManager.Connect("game", "127.0.0.1", 3306, "root", "")){
-        //     return;
-        // }
+        if(!DbManager.Connect("game", "127.0.0.1", 3306, "root", "")){
+            return;
+        }
 
         NetManager.StartLoop(8888);
     }
---
+++ FPSServer/Proto/CSHeartBeat.cs
@@ -0,0 +1,22 @@
+﻿using ProtoBuf;
+
+namespace FPSServer.Proto;
+
+[Serializable, ProtoContract(Name = @"CSHeartBeat")]
+public class CSHeartBeat : CSPacketBase
+{
+    public CSHeartBeat()
+    {
+        
+    }
+        
+    public override int Id => 1;
+
+    [ProtoMember (1)]
+    public string time{ get; set; }
+
+    public override void Clear()
+    {
+            
+    }
+}
